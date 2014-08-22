e092d1e4e5833ed791156a2540fa2c24a7ee17d4
Nicola Aitken <naitken22@gmail.com>
2014-08-22 15:11:48 +0100

    posts display on homepage

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
 
-describe 'iDontGeddits' do 
+describe 'posts' do 
 
-	context 'no iDontGeddits' do 
+	context 'no posts' do 
 
 		it 'shows a message' do 
 			visit '/posts'
 			expect(page).to have_content 'The internet is not confused at the moment. Are you?'
 		end
 
-	context 'one iDontGeddit' do 
+	context 'one post' do 
 
-		it 'shows one iDontGeddit when there is only one present' do 
+		it 'shows one post when there is only one present' do 
 			visit '/posts'
-			click_link "New iDontGeddit"
+			click_link "New DontGeddit"
 			fill_in "Title", with: "I just don't geddit"
 			fill_in "Text", with: "This is what I don't get"
+			click_button "Create DontGeddit"
 			expect(page).to have_content "I just don't geddit"
+			expect(page).to have_content "This is what I don't get"
 		end
 
 	end
